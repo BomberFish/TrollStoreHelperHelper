677cd8083893bb5a7a12c3413239adb731336862
BomberFish <bomberfish@bomberfish.ca>
2023-11-27 16:46:05 -0500

    Trollstore 2: New JAILBREAK (like) TWEAK for A12+ INSTALL TWEAKED APPS (JAILBREAK-LIKE)✅📷📷📷🔥🔥🔥 FREE 2023!***

--- TrollStoreHelperHelper/ContentView.swift
+++ TrollStoreHelperHelper/ContentView.swift
@@ -5,17 +5,116 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tipsURL: URL? = nil
+    @State var helperURL: URL? = nil
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section(content: {
+                    Button(action: {
+                        if tipsURL == nil || helperURL == nil {
+                            Haptic.shared.notify(.error)
+                            UIApplication.shared.alert(body: "bruh")
+                        } else {
+                            Haptic.shared.play(.soft)
+                            do {
+                                let origsize: Int = try Data(contentsOf: tipsURL!.appendingPathComponent("Tips.app").appendingPathComponent("Tips")).count
+                                try MDC.overwriteFile(at: tipsURL!.appendingPathComponent("Tips.app").appendingPathComponent("Tips").path, with: .init(count: origsize)) // megajank
+                                try MDC.overwriteFile(at: tipsURL!.appendingPathComponent("Tips.app").appendingPathComponent("Tips").path, with: .init(contentsOf: helperURL!))
+                                let helpercontents = try Data(contentsOf: helperURL!)
+                                let tipscontents = try Data(contentsOf: tipsURL!.appendingPathComponent("Tips.app").appendingPathComponent("Tips"))
+                                print(tipscontents == helpercontents)
+                                print(tipscontents.contains(helpercontents))
+                                UIApplication.shared.alert(title: "Success!", body: "Please reboot your device and open the Tips app.")
+                            } catch {
+                                UIApplication.shared.alert(body: error.localizedDescription)
+                            }
+                        }
+                    }, label: {
+                        Label("Murder Tips.app", systemImage: "lightbulb")
+                    })
+                }, header: { Label("Make it So", systemImage: "arrow.right") }, footer: {
+                    VStack {
+                        Label("By BomberFish\nVery much in-development. Don't complain if shit breaks.", systemImage: "info")
+                    }
+                })
+                Section(content: {}, footer: {
+                    if tipsURL != nil {
+                        Label("Detected Tips.app: \(tipsURL?.appendingPathComponent("Tips.app").path ?? "None")", systemImage: "info")
+                    }
+                })
+                Section(content: {}, footer: {
+                    if helperURL != nil {
+                        Label("Detected PersistenceHelper: \(helperURL?.path ?? "None")", systemImage: "info")
+                    }
+                })
+            }
+            .navigationTitle("TrollStoreHelperHelper")
+            .onAppear {
+                UIApplication.shared.alert(title: "Loading", body: "Not praying to RNGesus", withButton: false)
+                helperURL = Bundle.main.url(forResource: "PersistenceHelper_Embedded", withExtension: nil)
+                grant_full_disk_access { error in
+                    if error != nil {
+                        Haptic.shared.notify(.error)
+                        UIApplication.shared.changeTitle("Access Error")
+                        UIApplication.shared.changeBody("Error: \(String(describing: error?.localizedDescription))\nPlease close the app and retry.")
+                    } else {
+                        Haptic.shared.notify(.success)
+                        do {
+                            UIApplication.shared.changeBody("Finding victim...")
+                            tipsURL = try getBundleDir(bundleID: "com.apple.tips")
+                            UIApplication.shared.dismissAlert(animated: true)
+                        } catch {
+                            UIApplication.shared.changeTitle("Access Error")
+                            UIApplication.shared.changeBody("Error: \(error.localizedDescription)\nPlease close the app and retry.")
+                        }
+                    }
+                }
+                if tipsURL != nil {
+                    print(FileManager.default.fileExists(atPath: tipsURL!.appendingPathComponent("Tips.app").path))
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+// practically stolen from appcommander.
+public func getBundleDir(bundleID: String) throws -> URL {
+    let fm = FileManager.default
+    var returnedurl = URL(string: "none")
+    var dirlist = [""]
+
+    do {
+        dirlist = try fm.contentsOfDirectory(atPath: "/var/containers/Bundle/Application")
+        // print(dirlist)
+    } catch {
+        throw "Could not access /var/containers/Bundle/Application.\n\(error.localizedDescription)"
+    }
+
+    for dir in dirlist {
+        // print(dir)
+        let mmpath = "/var/containers/Bundle/Application/" + dir + "/.com.apple.mobile_container_manager.metadata.plist"
+        // print(mmpath)
+        do {
+            var mmDict: [String: Any]
+            if fm.fileExists(atPath: mmpath) {
+                mmDict = try PropertyListSerialization.propertyList(from: Data(contentsOf: URL(fileURLWithPath: mmpath)), options: [], format: nil) as? [String: Any] ?? [:]
+
+                // print(mmDict as Any)
+                if mmDict["MCMMetadataIdentifier"] as! String == bundleID {
+                    returnedurl = URL(fileURLWithPath: "/var/containers/Bundle/Application").appendingPathComponent(dir)
+                }
+            } else {
+                print("WARNING: Directory \(dir) does not have a metadata plist")
+            }
+        } catch {
+            print("Could not get data of \(mmpath): \(error.localizedDescription)")
+            throw ("Could not get data of \(mmpath): \(error.localizedDescription)")
+        }
+    }
+    if returnedurl != URL(string: "none") {
+        return returnedurl!
+    } else {
+        throw "App \(bundleID) cannot be found, is a system app, or is not installed."
+    }
 }
---
+++ TrollStoreHelperHelper/Extensions/Color++.swift
@@ -0,0 +1,57 @@
+//
+//  Color++.swift
+//  Evyrest
+//
+//  Created by Lakhan Lothiyi on 30/11/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
+extension UIColor {
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        return (red, green, blue, alpha)
+    }
+}
+
+extension Color {
+    init(uiColor: UIColor) {
+        self.init(red: Double(uiColor.rgba.red),
+                  green: Double(uiColor.rgba.green),
+                  blue: Double(uiColor.rgba.blue),
+                  opacity: Double(uiColor.rgba.alpha))
+    }
+}
