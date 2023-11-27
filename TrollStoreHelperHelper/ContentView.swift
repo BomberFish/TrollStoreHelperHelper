64bce4b2954a8fa95ccca9423443b0b903da169b
BomberFish <bomberfish@bomberfish.ca>
2023-11-27 15:17:13 -0500

    Initial Commit

---
+++ TrollStoreHelperHelper/ContentView.swift
@@ -0,0 +1,21 @@
+// bomberfish
+// ContentView.swift – TrollStoreHelperHelper
+// created on 2023-11-27
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrollStoreHelperHelper/TrollStoreHelperHelperApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// TrollStoreHelperHelperApp.swift – TrollStoreHelperHelper
+// created on 2023-11-27
+
+import SwiftUI
+
+@main
+struct TrollStoreHelperHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
